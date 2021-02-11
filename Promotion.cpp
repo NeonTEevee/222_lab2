104aa1ec2ebb7f83ec73a046c2af12dc84490e6d
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-02-11 15:21:51 +0700

    Fixed things

--- Promotion.cpp
+++ Promotion.cpp
@@ -5,19 +5,17 @@ int main ()
 	char promo;
 	float time, price, mins, secs;
 	scanf("%c", &promo);
-	scanf("%0.2f", &time);
+	scanf("%f", &time);
 	
 	secs = time-floor(time);
-	printf("%f\n", secs);
 	mins = floor(time);
-	printf("%f\n", mins);
 	
 	if(promo == 'A')
 	{
-		printf("%0.2f", 199+(((mins-200)+(secs/60))*3));
+		printf("%0.2f", 199+(((mins-200)+(secs*100/60))*3));
 	}
 	if(promo == 'B')
 	{
-		printf("%0.2f", 299+(((mins-400)+(secs/60))*2));
+		printf("%0.2f", 299+(((mins-400)+(secs*100/60))*2));
 	}
 }
