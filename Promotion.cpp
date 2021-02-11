c57ea4b49ca66338813211446aeb2f56a2ad7e41
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-02-11 15:16:32 +0700

    Finished programming

---
+++ Promotion.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+	char promo;
+	float time, price, mins, secs;
+	scanf("%c", &promo);
+	scanf("%0.2f", &time);
+	
+	secs = time-floor(time);
+	mins = floor(time);
+	
+	if(promo == 'A')
+	{
+		printf("%0.2f", 199+(((mins-200)+(secs/60))*3));
+	}
+	if(promo == 'B')
+	{
+		printf("%0.2f", 299+(((mins-400)+(secs/60))*2));
+	}
+}
