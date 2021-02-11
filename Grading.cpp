afcbae2839af756f0081955d0ad26962ded686dd
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-02-11 14:56:27 +0700

    Tried changing a small thing again

--- Grading.cpp
+++ Grading.cpp
@@ -2,7 +2,9 @@
 int main ()
 {
 	int a, b, c, sum;
-	scanf("%d %d %d", &a, &b, &c);
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
 	sum = a + b + c;
 	if(sum >= 80 && sum <= 100)
 		printf("A");
